939e37474cdff71f3d7e019e1b14b6aff76f7d70
DIYNG-DISEASE\Bass <basdxz>
2016-10-09 20:35:15 +0100

    Blank Cube test

---
+++ holofailure.lua
@@ -0,0 +1,39 @@
+--Variables and functions
+--Layer 1 {OK, Fault, Crit} Layer 2 {A,B,C} Layer 3 {Red 0-255, Green 0-255, Blue 0-255,}
+
+local colours = {
+	{{0,0,255},{0,0,255},{0,0,255}},
+	{{0,0,255},{0,0,255},{0,0,255}}, 
+	{{0,0,255},{0,0,255},{0,0,255}}} 
+
+function EZCONCAT(tab)
+    local txt--=""
+    for local i=1,xMax do
+        for local j=1,yMax do
+            for local k=1,zMax do
+                txt..tab[i][j][k]
+    end end end
+    return txt
+end
+
+--Load Basics
+
+local component = require("component")
+local keyboard =  require("keyboard")
+local holo = component.hologram
+holo.clear()
+--Define 3D frame
+
+local frame, mFrame = {}, {}
+local mFrame.__index=function() return "\0" end
+setmetatable(frame,mFrame)
+
+
+
+
+holo.setRaw(EZCONCAT(frame))
+
+
+function MyFunction(Str)
+
+end
